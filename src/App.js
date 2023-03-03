b8a79f7141a78e0f7b67806ee0e6a1383ed665db
Karina Ostrovska <karina.ostrovsika@gmail.com>
2023-03-03 23:35:27 +0200

    work with main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import React from 'react';
+import Header from './Components/Header';
+import Categories from './Components/Categories';
+import Sort from './Components/Sort';
+import PizzaBlock from './Components/PizzaBlock';
+
+function Pizza(){
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header/>
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories/>
+            <Sort/>
+          </div>
+          <h2 class="content__title">All pizzas</h2>
+          <div class="content__items">
+              <PizzaBlock price={129} title='Classic pizza'/>
+              <PizzaBlock price={189} title='Meat pizza'/>
+              <PizzaBlock price={119} title='Cheese pizza'/>
+              <PizzaBlock price={119} title='Margarita'/>
+              <PizzaBlock price={139} title='Specialty pizza'/>
+              <PizzaBlock price={129} title='Vegetarian pizza'/>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/Components/Categories.jsx
@@ -0,0 +1,18 @@
+
+
+const Categories = () => {
+    return(
+        <div class="categories">
+            <ul>
+                <li class="active">Everything</li>
+                <li>Meat</li>
+                <li>Vegetarian</li>
+                <li>Grill</li>
+                <li>Spicy</li>
+                <li>Closed</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Categories
