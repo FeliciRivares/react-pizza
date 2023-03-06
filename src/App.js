297bcd2f66db533f68d68aec670ffe41e6dc2e96
Karina Ostrovska <karina.ostrovsika@gmail.com>
2023-03-07 00:46:47 +0200

    create page cart, home, notFound use Route and Link to travel between pages

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import './scss/app.scss';
 import React from 'react';
 import Header from './Components/Header';
-import Categories from './Components/Categories';
-import Sort from './Components/Sort';
-import PizzaBlock from './Components/PizzaBlock';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import { Route, Routes } from 'react-router-dom';
+import Cart from './pages/Cart';
+
 
 function App() {
-const [state, setState] = React.useState([]);
-
-React.useEffect(() => {
-  fetch('https://6405e93740597b65de44db55.mockapi.io/items')
-  .then((res) => res.json())
-  .then((json) => setState(json))
-}, [])
-
   return (
     <div className="App">
-    <div className="wrapper">
-      <Header/>
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories/>
-            <Sort />
-          </div>
-          <h2 className="content__title">All pizzas</h2>
-          <div className="content__items">
-              {state.map((obj) => (
-                  <PizzaBlock key={obj.id} {...obj}/>
-                ))}
-          </div>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+            <Routes>
+             <Route path='/' element={<Home/>}/> 
+             <Route path='/cart' element={<Cart/>}/>
+             <Route path='*' element={<NotFound/>}/>
+            </Routes>
         </div>
       </div>
-    </div>
     </div>
   );
 }
